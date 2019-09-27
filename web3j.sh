566d188db918c5a11d3c0e851e8d8ec1c4a3addf
alexander <alexandrosrousos@gmail.com>
2019-09-27 15:40:54 +0100

    .

--- web3j.sh
+++ web3j.sh
@@ -31,7 +31,7 @@ download_web3j() {
   mkdir "${local}.web3j"
   wget -P "${local}.web3j" "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.zip" -q
   unzip -o "${local}.web3j/web3j-${web3j_version}.zip" -d "${local}.web3j"
-  
+  rm "${local}.web3j/web3j-${web3j_version}.zip"
 }
 
 check_version() {
