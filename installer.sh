f43fff116af38e441e7fcd0280e4c92b4e13ea62
Rousos Alexandros <alexandrosrousos@gmail.com>
2019-10-04 18:26:45 +0100

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 local=~/
-
+web3j_version="4.5.5"
 clean_up() {
 if [ -d "${local}.web3j" ]; then
 echo "Looks like the .web3j folder exists in your local directory."
@@ -10,12 +9,74 @@ echo "Creating web3j directory at path ${local}.web3j"
 mkdir "${local}.web3j"
 fi
 }
+set_path() {
+
+  if (test -f "${HOME}/.bash_profile" && ! grep -s --quiet ".web3j/web3j-${web3j_version}" "$HOME/.bash_profile"); then
+    echo "export PATH=\$PATH:~/.web3j/web3j-${web3j_version}/bin" >> ~/.bash_profile
+    echo "Web3j has been added to your bash_profile path variable."
+    
+   else 
+    echo "Web3j path exists in bash_profile"
+   fi
+  if (test -f "${HOME}/.bashrc" && ! grep -s --quiet ".web3j/web3j-${web3j_version}" "$HOME/.bashrc"); then
+    echo "export PATH=\$PATH:~/.web3j/web3j-${web3j_version}/bin" >> ~/.bashrc
+    echo "Web3j has been added to your bashrc path variable."   
+   
+  else 
+     echo "Web3j path exists in bashrc"
+  fi
+  if (test -f "${HOME}/.zshrc" && ! grep -s --quiet ".web3j/web3j-${web3j_version}" "$HOME/.zshrc"); then
+    echo "export PATH=\$PATH:~/.web3j/web3j-${web3j_version}/bin" >> ~/.zshrc
+    echo "Web3j has been added to your zshrc path variable."
+   
+  else
+     echo "Web3j path exists in zshrc"
+  fi
+  {
+    echo "export PATH=\$PATH:$HOME/.web3j/web3j-${web3j_version}/bin"
+  } > ~/.web3j/path-update.sh 
+  chmod 777 ~/.web3j/path-update.sh 
+}
+
+add_to_path() {
+  while read -p "Would you like to add Web3j to your local path ? [y]es | [n]o : " user_input < /dev/tty ; do
+   case $user_input in
+    y)
+      set_path
+      break
+      ;;
+    n)
+      echo "Web3j was not added to path. Path to binary: ~/web3j/web3j-${web3j_version}/bin/web3j"
+      break
+      ;;
+    esac
+  done
+}
+
+completed() {
+  echo "Web3j was succesfully installed"
+  echo "To get started you will need Web3j's bin directory in your PATH enviroment variable."
+  echo "When you open a new terminal window this will be done automatically." 
+  echo "To see what web3j's CLI can do you can check the documentation bellow."
+  echo "https://docs.web3j.io/command_line_tools/ "
+  echo "To use web3j in your current shell run source \$HOME/.web3j/path-update.sh "
+  exit 0
+}
 
 download_installer() {
-if [[ $(curl --write-out %{http_code} --silent --output /dev/null "https://raw.githubusercontent.com/web3j/web3j-installer/master/web3j.sh") -eq 200 ]] ; then
+if [[ $(curl --write-out %{http_code} --silent --output /dev/null "https://raw.githubusercontent.com/AlexandrouR/web3j-installer/master/web3j.sh") -eq 200 ]] ; then
     echo "Downloading install script ..."
-    curl -# -L -o ~/.web3j/web3j.sh "https://raw.githubusercontent.com/web3j/web3j-installer/master/web3j.sh"
-    chmod 777 ~/.web3j/web3j.sh 
+    curl -# -L -o ~/.web3j/web3j.sh "https://raw.githubusercontent.com/AlexandrouR/web3j-installer/master/web3j.sh"
+    chmod 777 ~/.web3j/web3j.sh  
+    ~/.web3j/web3j.sh < /dev/tty
+    if [ $? = 3 ] ; then
+
+    exit 0 
+    else 
+    add_to_path 
+    completed
+    fi
+
  else
   echo "Looks like there was an error while trying to get the web3j install script."
   exit 0
--- web3j.sh
+++ web3j.sh
@@ -6,59 +6,24 @@ local=~/
 
 check_if_installed() {
   if [ -x "$(command -v web3j)" ] &>/dev/null; then
-    echo 'Web3j is installed on you system.'
+    echo 'A web3j installation exists on your system.'
     installed_flag=1
   fi
 }
 
-completed() {
-  echo "To start using the CLI type web3j"
-  web3j 
-}
 
-set_path() {
-
-  if (test -f "${HOME}/.bash_profile" && ! grep -s --quiet ".web3j/web3j-${web3j_version}" "$HOME/.bash_profile"); then
-    echo "export PATH=\$PATH:~/.web3j/web3j-${web3j_version}/bin" >> ~/.bash_profile
-    echo "Web3j has been added to your bash_profile path variable."
-    
-   else 
-    echo "Web3j path exists in bash_profile"
-   fi
-  if (test -f "${HOME}/.bashrc" && ! grep -s --quiet ".web3j/web3j-${web3j_version}" "$HOME/.bashrc"); then
-    echo "export PATH=\$PATH:~/.web3j/web3j-${web3j_version}/bin" >> ~/.bashrc
-    echo "Web3j has been added to your bashrc path variable."   
-   
-  else 
-     echo "Web3j path exists in bashrc"
-  fi
-  if (test -f "${HOME}/.zshrc" && ! grep -s --quiet ".web3j/web3j-${web3j_version}" "$HOME/.zshrc"); then
-    echo "export PATH=\$PATH:~/.web3j/web3j-${web3j_version}/bin" >> ~/.zshrc
-    echo "Web3j has been added to your zshrc path variable."
-   
-  else
-     echo "Web3j path exists in zshrc"
-  fi
-  
-  {
-  echo "export PATH=\$PATH:~/.web3j/web3j-${web3j_version}/bin"
-  } > ~/.web3j/source.sh
-  source ~/.web3j/source.sh
-
-   
-}
 
 download_web3j() {
   echo "Downloading Web3j ..."
-  mkdir "${local}.web3j"
+  mkdir "${local}.web3j" &>/dev/null
  if [[ $(curl --write-out %{http_code} --silent --output /dev/null "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.zip") -eq 302 ]] ; then
-    curl -# -L -o ~/.web3j/web3j-${web3j_version}.zip "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.zip"
+    curl -# -L -o ~/.web3j/web3j-${web3j_version}.zip "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.zip" 
     unzip  -o "${local}.web3j/web3j-${web3j_version}.zip" -d "${local}.web3j"
     echo "Removing zip file ..."
     rm "${local}.web3j/web3j-${web3j_version}.zip"
  else
   echo "Looks like there was an error while trying to download web3j"
-  exit 0
+  exit 3
  fi
 }
 
@@ -70,13 +35,13 @@ check_version() {
     get_user_input
   else
     echo "You have the latest version of Web3j. Exiting."
-    exit 0
+    exit 3
   fi
 }
 
 get_user_input() {
 
-  while read -p "Would you like to update Web3j ? [y]es | [n]o : " user_input ; do
+  while read -p "Would you like to update Web3j ? [y]es | [n]o : " user_input < /dev/tty; do
     case $user_input in
     y)
       echo "Updating Web3j ..."
@@ -85,31 +50,18 @@ get_user_input() {
       ;;
     n)
       echo "Aborting instalation ..."
-      exit 0
+      exit 3
       ;;
     esac
   done
 }
 
-add_to_path() {
-  while read -p "Would you like to add Web3j to your local path ? [y]es | [n]o : " user_input ; do
-   case $user_input in
-    y)
-      set_path
-      break
-      ;;
-    n)
-      echo "Web3j was not added to path. Path to binary: ~/web3j/web3j-${web3j_version}/bin/web3j"
-      exit 0
-      ;;
-    esac
-  done
-}
+
 
 check_if_web3j_homebrew() {
   if (command -v brew && ! (brew info web3j | grep "Not installed") &>/dev/null); then
     echo "Looks like Web3j is installed with Homebrew. Please use Homebrew to update. Exiting."
-    exit 0
+    exit 3
   fi
 }
 
@@ -127,13 +79,10 @@ main() {
     check_version
     clean_up
     download_web3j
-    add_to_path
-    completed
+    
   else
-    clean_up
     download_web3j
-    add_to_path
-    completed
+    
   fi
 }
 
