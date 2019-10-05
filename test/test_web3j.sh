638237342ff044b02131aeaf3ced84a7bc31e3f3
Joshua Richardson <joshua@richardson.tech>
2019-10-05 23:30:35 +0100

    Removing unneccesary java stuff

--- test/test_web3j.sh
+++ test/test_web3j.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
-JAVA_EXECUTABLE=$(readlink -f $(which java))
-export JAVA_HOME=$(dirname $(dirname $JAVA_EXECUTABLE))
-echo $JAVA_HOME
 bash installer.sh
 echo "Web3j source script content:"
 cat $HOME/.web3j/source.sh
