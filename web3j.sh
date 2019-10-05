3f76d9965582b9a9b22eae9e677f1d1a06521198
Joshua Richardson <joshua@richardson.tech>
2019-10-05 01:21:47 +0100

    Make installer functional, allow for adding to bashrc automatically (as in jabba, nvm, fnm, etc)

--- web3j.sh
+++ web3j.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 web3j_version="4.5.5"
 installed_flag=0
-update_flag=0
-local=~/
 
 check_if_installed() {
   if [ -x "$(command -v web3j)" ] &>/dev/null; then
@@ -11,26 +9,26 @@ check_if_installed() {
   fi
 }
 
-
-
-download_web3j() {
+install_web3j() {
   echo "Downloading Web3j ..."
-  mkdir "${local}.web3j"
- if [[ $(curl --write-out %{http_code} --silent --output /dev/null "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.zip") -eq 302 ]] ; then
-    curl -# -L -o ~/.web3j/web3j-${web3j_version}.zip "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.zip"
-    unzip  -o "${local}.web3j/web3j-${web3j_version}.zip" -d "${local}.web3j"
+  mkdir "$HOME/.web3j"
+  if [[ $(curl --write-out %{http_code} --silent --output /dev/null "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.zip") -eq 302 ]]; then
+    curl -# -L -o "$HOME/.web3j/web3j-${web3j_version}.zip" "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.zip"
+    unzip -o "$HOME/.web3j/web3j-${web3j_version}.zip" -d "$HOME/.web3j"
+    echo "export PATH=\$PATH:$HOME/.web3j/web3j-${web3j_version}/bin" >"$HOME/.web3j/source.sh"
+    chmod +x "$HOME/.web3j/source.sh"
     echo "Removing zip file ..."
-    rm "${local}.web3j/web3j-${web3j_version}.zip"
- else
-  echo "Looks like there was an error while trying to download web3j"
-  exit 0
- fi
+    rm "$HOME/.web3j/web3j-${web3j_version}.zip"
+  else
+    echo "Looks like there was an error while trying to download web3j"
+    exit 0
+  fi
 }
 
 check_version() {
-  version_string=`web3j version | grep Version | awk -F" "  '{print $NF}'`
-  echo $version_string
-  if [[ $version_string < $web3j_version ]] ; then
+  version_string=$(web3j version | grep Version | awk -F" " '{print $NF}')
+  echo "$version_string"
+  if [[ $version_string < $web3j_version ]]; then
     echo "Your Web3j version is not up to date."
     get_user_input
   else
@@ -40,12 +38,10 @@ check_version() {
 }
 
 get_user_input() {
-
-  while read -p "Would you like to update Web3j ? [y]es | [n]o : " user_input ; do
+  while read -p "Would you like to update Web3j ? [y]es | [n]o : " user_input; do
     case $user_input in
     y)
       echo "Updating Web3j ..."
-      update_flag=1
       break
       ;;
     n)
@@ -56,8 +52,6 @@ get_user_input() {
   done
 }
 
-
-
 check_if_web3j_homebrew() {
   if (command -v brew && ! (brew info web3j | grep "Not installed") &>/dev/null); then
     echo "Looks like Web3j is installed with Homebrew. Please use Homebrew to update. Exiting."
@@ -66,9 +60,46 @@ check_if_web3j_homebrew() {
 }
 
 clean_up() {
-  if [ -d "${local}.web3j" ] ; then
-  rm -r "${local}.web3j" &>/dev/null
-  echo "Deleting older installation ..."
+  if [ -d "$HOME/.web3j" ]; then
+    rm -r "$HOME/.web3j" &>/dev/null
+    echo "Deleting older installation ..."
+  fi
+}
+
+source_web3j() {
+  SOURCE_WEB3J="\n[ -s \"$HOME/.web3j/source.sh\" ] && source \"$HOME/.web3j/source.sh\""
+
+  files=("$HOME/.bashrc")
+
+  if [ -f "$HOME/.bash_profile" ]; then
+    files+=("$HOME/.bash_profile")
+  elif [ -f "$HOME/.bash_login" ]; then
+    files+=("$HOME/.bash_login")
+  elif [ -f "$HOME/.profile" ]; then
+    files+=("$HOME/.profile")
+  else
+    files+=("$HOME/.bash_profile")
+  fi
+
+  for file in "${files[@]}"; do
+    touch "${file}"
+    if ! grep -qc '.web3j/source.sh' "${file}"; then
+      echo "Adding source string to ${file}"
+      printf "$SOURCE_WEB3J\n" >>"${file}"
+    else
+      echo "Skipped update of ${file} (source string already present)"
+    fi
+  done
+
+  if [ -f "$(command -v zsh 2>/dev/null)" ]; then
+    file="$HOME/.zshrc"
+    touch ${file}
+    if ! grep -qc '.web3j/source.sh' "${file}"; then
+      echo "Adding source string to ${file}"
+      printf "$SOURCE_WEB3J\n" >>"${file}"
+    else
+      echo "Skipped update of ${file} (source string already present)"
+    fi
   fi
 }
 
@@ -78,12 +109,12 @@ main() {
     check_if_web3j_homebrew
     check_version
     clean_up
-    download_web3j
-
+    install_web3j
+    source_web3j
   else
     clean_up
-    download_web3j
-
+    install_web3j
+    source_web3j
   fi
 }
 
