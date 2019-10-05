45793b01df6cb469c9722d6656c5b880730b7991
Joshua Richardson <joshua@richardson.tech>
2019-10-06 00:19:33 +0100

    negative test for powershell

--- installer.ps1
+++ installer.ps1
@@ -7,7 +7,7 @@ New-Item -Force -ItemType directory -Path "${env:USERPROFILE}\.web3j" | Out-Null
 $url = "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.zip"
 $output = "${env:USERPROFILE}\.web3j\web3j.zip"
 Write-Output "Downloading Web3j version ${web3j_version}..."
-Invoke-WebRequest -Uri $url -OutFile $output
+Invoke-WebRequest -Ui $url -OutFile $output
 Write-Output "Extracting Web3j..."
 Expand-Archive -Path "${env:USERPROFILE}\.web3j\web3j.zip" -DestinationPath "${env:USERPROFILE}\.web3j\" -Force
 $CurrentPath = [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User)
