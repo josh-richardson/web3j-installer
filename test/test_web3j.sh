2d87fe68c74dbeeecae8c08727283513c6288514
Joshua Richardson <joshua@richardson.tech>
2019-10-05 23:27:11 +0100

    more travis debug

--- test/test_web3j.sh
+++ test/test_web3j.sh
@@ -4,7 +4,12 @@ JAVA_EXECUTABLE=$(readlink -f $(which java))
 export JAVA_HOME=$(dirname $(dirname $JAVA_EXECUTABLE))
 echo $JAVA_HOME
 bash installer.sh
-ls
-source ~/.bashrc
+echo "Web3j source script content:"
+cat $HOME/.web3j/source.sh
+echo "Sourcing web3j source script"
+source $HOME/.web3j/source.sh
+echo "Content of bashrc:"
+cat ~/.bashrc
+echo "System path:"
 echo $PATH
 web3j version
