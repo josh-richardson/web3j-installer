5103326087dfaa0a64be4825a54db5e367783ff6
Joshua Richardson <joshua@richardson.tech>
2019-10-06 00:26:50 +0100

    Test web3j works on windows after install

--- test/test_web3j.sh
+++ test/test_web3j.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 if [ -f "C:\\windows\\system32\\drivers\\etc\\hosts" ]; then
+#  choco install jdk8
   powershell -executionpolicy bypass .\\installer.ps1
+  ~/.web3j/bin/web3j.bat version
   exit 0
 fi
+
 bash installer.sh
 echo "Web3j source script content:"
 cat $HOME/.web3j/source.sh
