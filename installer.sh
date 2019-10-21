32324e10d3246aa73740485aab220035bb461524
Rousos Alexandros <alexandrosrousos@gmail.com>
2019-10-21 10:51:33 +0100

    Changed script to be posix compatable  (#10)
    * Create posix_compatable.sh
    
    * Update posix_compatable.sh
    
    * Update posix_compatable.sh
    
    * Delete installer.sh
    
    * Rename posix_compatable.sh to installer.sh
    
    * Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 web3j_version="4.5.5"
 installed_flag=0
-
 check_if_installed() {
-  if [ -x "$(command -v web3j)" ] &>/dev/null; then
+  if [ -x "$(command -v web3j)" ] >/dev/null 2>&1; then
     printf 'A Web3j installation exists on your system: '
     installed_flag=1
   fi
@@ -12,7 +11,7 @@ check_if_installed() {
 install_web3j() {
   echo "Downloading Web3j ..."
   mkdir "$HOME/.web3j"
-  if [[ $(curl --write-out %{http_code} --silent --output /dev/null "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.tar") -eq 302 ]]; then
+  if [ "$(curl --write-out "%{http_code}" --silent --output /dev/null "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.tar")" -eq 302 ]; then
     curl -# -L -o "$HOME/.web3j/web3j-${web3j_version}.tar" "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.tar"
     echo "Installing Web3j..."
     tar -xf "$HOME/.web3j/web3j-${web3j_version}.tar" -C "$HOME/.web3j"
@@ -29,7 +28,7 @@ install_web3j() {
 check_version() {
   version_string=$(web3j version | grep Version | awk -F" " '{print $NF}')
   echo "$version_string"
-  if [[ $version_string < $web3j_version ]]; then
+  if [ "$version_string" = "$web3j_version" ]; then
     echo "Your Web3j version is not up to date."
     get_user_input
   else
@@ -39,7 +38,7 @@ check_version() {
 }
 
 get_user_input() {
-  while read -p "Would you like to update Web3j ? [y]es | [n]o : " user_input; do
+  while read "Would you like to update Web3j ? [y]es | [n]o : " user_input; do
     case $user_input in
     y)
       echo "Updating Web3j ..."
@@ -52,37 +51,48 @@ get_user_input() {
     esac
   done
 }
-
-check_if_web3j_homebrew() {
-  if (command -v brew && ! (brew info web3j | grep "Not installed") &>/dev/null); then
-    echo "Looks like Web3j is installed with Homebrew. Please use Homebrew to update. Exiting."
-    exit 0
-  fi
-}
-
-clean_up() {
-  if [ -d "$HOME/.web3j" ]; then
-    rm -r "$HOME/.web3j" &>/dev/null
-    echo "Deleting older installation ..."
-  fi
-}
-
 source_web3j() {
   SOURCE_WEB3J="\n[ -s \"$HOME/.web3j/source.sh\" ] && source \"$HOME/.web3j/source.sh\""
-
-  files=("$HOME/.bashrc")
-
-  if [ -f "$HOME/.bash_profile" ]; then
-    files+=("$HOME/.bash_profile")
+  if [ -f "$HOME/.bashrc" ]; then
+    bash_rc="$HOME/.bashrc"
+    touch "${bash_rc}"
+    if ! grep -qc '.web3j/source.sh' "${bash_rc}"; then
+      echo "Adding source string to ${bash_rc}"
+      printf "${SOURCE_WEB3J}\n" >>"${bash_rc}"
+    else
+      echo "Skipped update of ${bash_rc} (source string already present)"
+    fi
+  elif [ -f "$HOME/.bash_profile" ]; then
+    bash_profile="${HOME}/.bash_profile"
+    touch "${bash_profile}"
+    if ! grep -qc '.web3j/source.sh' "${bash_profile}"; then
+      echo "Adding source string to ${bash_profile}"
+      printf "${SOURCE_WEB3J}\n" >>"${bash_profile}"
+    else
+      echo "Skipped update of ${bash_profile} (source string already present)"
+    fi
   elif [ -f "$HOME/.bash_login" ]; then
-    files+=("$HOME/.bash_login")
+    bash_login="$HOME/.bash_login"
+    touch "${bash_login}"
+    if ! grep -qc '.web3j/source.sh' "${bash_login}"; then
+      echo "Adding source string to ${bash_login}"
+      printf "${SOURCE_WEB3J}\n" >>"${bash_login}"
+    else
+      echo "Skipped update of ${bash_login} (source string already present)"
+    fi
   elif [ -f "$HOME/.profile" ]; then
-    files+=("$HOME/.profile")
-  else
-    files+=("$HOME/.bash_profile")
+    profile="$HOME/.profile"
+    touch "${profile}"
+    if ! grep -qc '.web3j/source.sh' "${profile}"; then
+      echo "Adding source string to ${profile}"
+      printf "$SOURCE_WEB3J\n" >>"${profile}"
+    else
+      echo "Skipped update of ${profile} (source string already present)"
+    fi
   fi
 
-  for file in "${files[@]}"; do
+  if [ -f "$(command -v zsh 2>/dev/null)" ]; then
+    file="$HOME/.zshrc"
     touch "${file}"
     if ! grep -qc '.web3j/source.sh' "${file}"; then
       echo "Adding source string to ${file}"
@@ -90,24 +100,27 @@ source_web3j() {
     else
       echo "Skipped update of ${file} (source string already present)"
     fi
-  done
-
-  if [ -f "$(command -v zsh 2>/dev/null)" ]; then
-    file="$HOME/.zshrc"
-    touch ${file}
-    if ! grep -qc '.web3j/source.sh' "${file}"; then
-      echo "Adding source string to ${file}"
-      printf "$SOURCE_WEB3J\n" >>"${file}"
-    else
-      echo "Skipped update of ${file} (source string already present)"
-    fi
   fi
 }
+check_if_web3j_homebrew() {
+  if (command -v brew && ! (brew info web3j | grep "Not installed") >/dev/null 2>&1); then
+    echo "Looks like Web3j is installed with Homebrew. Please use Homebrew to update. Exiting."
+    exit 0
+  fi
+}
+
+clean_up() {
+  if [ -d "$HOME/.web3j" ]; then
+    rm -r "$HOME/.web3j" >/dev/null 2>&1
+    echo "Deleting older installation ..."
+  fi
+}
+
 completed() {
   printf '\033[32m'
   echo "Web3j was succesfully installed"
   echo "To get started you will need Web3j's bin directory in your PATH enviroment variable."
-  echo "When you open a new terminal window this will be done automatically." 
+  echo "When you open a new terminal window this will be done automatically."
   echo "To see what web3j's CLI can do you can check the documentation bellow."
   echo "https://docs.web3j.io/command_line_tools/ "
   echo "To use web3j in your current shell run:"
@@ -124,11 +137,13 @@ main() {
     install_web3j
     source_web3j
     completed
+   
   else
     clean_up
     install_web3j
     source_web3j
     completed
+    
   fi
 }
 
