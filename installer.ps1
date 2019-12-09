8f3474a6d14150b5e2bf705571d05f49507c1a1a
Joshua Richardson <joshua@richardson.tech>
2019-12-09 15:22:22 +0000

    Curse you, microsoft

--- installer.ps1
+++ installer.ps1
@@ -4,7 +4,7 @@ $PSDefaultParameterValues['*:ErrorAction']='Stop'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $ProgressPreference = 'SilentlyContinue'
 
-$web3j_version = $(Invoke-WebRequest -Uri https://internal.services.web3labs.com/api/versions/latest).content
+$web3j_version = $(Invoke-WebRequest -UseBasicParsing -Uri https://internal.services.web3labs.com/api/versions/latest).content
 
 New-Item -Force -ItemType directory -Path "${env:USERPROFILE}\.web3j" | Out-Null
 $url = "https://github.com/web3j/web3j-cli/releases/download/v${web3j_version}/web3j-${web3j_version}.zip"
